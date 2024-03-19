c6fdd122ff9ebf3f590bcafe5c753eabd8fe4ac9
RidhwanFachrul <146655228+RidhwanFachrul@users.noreply.github.com>
2024-03-20 00:00:09 +0700

    commit

---
+++ bismillah/routes/web.php
@@ -0,0 +1,31 @@
+//
+<?php
+ 
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+ 
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+
+});
+
+Route::resource("/student", StudentController::class);
+
+
